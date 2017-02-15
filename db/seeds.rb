17187858aca3e31c66f9f0b3ed03931048b81e1e
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-02-15 12:29:45 +0000

    restaurant controller added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  name = Faker::Company.name
+  address = "#{Faker::Address.street_address} #{Faker::Address.zip} #{Faker::Address.city}"
+  phone = Faker::PhoneNumber.phone_number
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  Restaurant.create(name: name, address: address, phone: phone, category: category)
+end
