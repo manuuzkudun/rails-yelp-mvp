81c25e534b4a2183488a1cffc21edafb553736fc
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-02-15 16:02:18 +0000

    validations implemented

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    res = @restaurants[0].reviews
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,10 @@ ActiveRecord::Schema.define(version: 20170215125224) do
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
-    t.string   "phone"
+    t.string   "phone_number"
     t.string   "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "reviews", force: :cascade do |t|
